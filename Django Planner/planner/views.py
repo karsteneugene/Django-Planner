3a25d7a373b7465caab9a2db8b6e2663418ac801
karsteneugene <karsten.lie@binus.ac.id>
2020-12-16 21:55:48 +0700

    Added routes, HTML templates, and CSS

--- Django Planner/planner/views.py
+++ Django Planner/planner/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+to_do_list = [
+    {
+        'day': '25 December',
+        'to_do_1': 'Grab breakfast',
+        'to_do_2': 'Meet up friends',
+        'to_do_3': 'Work out',
+        'notes': 'Remember, you are on a diet!'
+    },
+    {
+        'day': '26 December',
+        'to_do_1': 'Grab breakfast',
+        'to_do_2': 'Complete project',
+        'to_do_3': 'Walk dog',
+        'notes': 'Better finish project early!'
+    }
+]
+
+
+def home(request):
+    context = {
+        'to_dos': to_do_list
+    }
+    return render(request, 'planner/home.html', context)
+
+
+def calendar(request):
+    return render(request, 'planner/calendar.html', {'title': 'Calendar'})
