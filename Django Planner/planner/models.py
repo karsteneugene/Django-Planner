43774ea15a5dd52e80e8310be12e5e622816a94f
karsteneugene <karsten.lie@binus.ac.id>
2020-12-24 20:47:03 +0700

    Added database for Tasks

--- Django Planner/planner/models.py
+++ Django Planner/planner/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    type = models.CharField(max_length=20)
+    date_assigned = models.DateField()
+    date_due = models.DateField()
+    course = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- Django Planner/planner/views.py
+++ Django Planner/planner/views.py
@@ -1,37 +1,10 @@
 from django.shortcuts import render
-
-
-tasks = [
-    {
-        'title': 'Python Programming Exercise 12',
-        'type': 'Assignment',
-        'date_assigned': '1 December',
-        'date_due': '8 December',
-        'course': 'Program Design Methods',
-        'description': '<Objective for this task>'
-    },
-    {
-        'title': 'Regular Expressions',
-        'type': 'Assignment',
-        'date_assigned': '5 December',
-        'date_due': '12 December',
-        'course': 'Discrete Structures',
-        'description': '<Objective for this task>'
-    },
-    {
-        'title': 'Alien Invasion',
-        'type': 'Project',
-        'date_assigned': '1 December',
-        'date_due': '1 January',
-        'course': 'Program Design Methods',
-        'description': '<Objective for this task>'
-    }
-]
+from .models import Task
 
 
 def dashboard(request):
     context = {
-        'tasks': tasks
+        'tasks': Task.objects.all()
     }
     return render(request, 'planner/dashboard.html', context)
 
