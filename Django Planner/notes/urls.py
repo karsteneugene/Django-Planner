af1a2fa8cca4256bd0c86b39fd2e95b65558ba5d
karsteneugene <karsten.lie@binus.ac.id>
2020-12-29 21:07:06 +0700

    Skip delete confirmation for task and courses

--- Django Planner/notes/urls.py
+++ Django Planner/notes/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('', NotesListView.as_view(), name='notes'),
     path('<int:pk>/', NotesDetailView.as_view(), name='notes-detail'),
     path('new/', NotesCreateView.as_view(), name='notes-create'),
-    path('/<int:pk>/update/', NotesUpdateView.as_view(), name='notes-update'),
-    path('/<int:pk>/delete/', NotesDeleteView.as_view(), name='notes-delete'),
+    path('<int:pk>/update/', NotesUpdateView.as_view(), name='notes-update'),
+    path('<int:pk>/delete/', NotesDeleteView.as_view(), name='notes-delete'),
 ]
